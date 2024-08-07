4986ff94e3c299591fe7d4255a831b04b6818e8c
Umakant Dwivedi <141998767+Umakant04@users.noreply.github.com>
2024-08-07 10:35:28 +0000

    learn varaibles

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1443
+let accountEmail = "umkasvsv@gmail.com"
+accountCity = "jaipur"
+var accountPassword = "123456"
+
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountCity, accountPassword])
+
