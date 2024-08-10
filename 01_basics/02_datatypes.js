952b4b596980a2c864a3a73568022de7bd3ef234
Umakant Dwivedi <141998767+Umakant04@users.noreply.github.com>
2024-08-10 08:46:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+console.log("umkaanat");
+
+let age = 18;
+let isLoggedIn = false;
+let name = "umakant"
+
+
+// datatype in js 
+
+// int 
+//string: prefer in double quotes " "
+//bigint: it use when your is very big it not easy to handle with int 
+//null: standalone value
+//boolean : true/false
+// undefined: it defined without value
+
+
+console.log(typeof "hitesh");
+
+console.log(typeof undefined);
+console.log(typeof  null);
+
+
