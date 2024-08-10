6f2dc3cb72ca4b22fbb9baf55c9c235696aff5bb
Umakant Dwivedi <141998767+Umakant04@users.noreply.github.com>
2024-08-10 09:55:59 +0000

    conversion operator

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,14 @@
+let score  = "33abfbfb";
+// let score  = null;
+
+//console.log(typeof score);
+
+let valueinNumber = Number(score)
+
+console.log(typeof valueinNumber);
+
+console.log( valueinNumber);
+
+
+
+
